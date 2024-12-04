495f8c4535b3d2cdfce7e0cf52c061ebeaba6cea
Bara Ardiwinata <162680348+BaraArdiwinata@users.noreply.github.com>
2024-12-04 23:39:09 +0700

    Monster.java

--- Monster.java
+++ Monster.java
@@ -1,14 +1,15 @@
 public class Monster {
     private int id; // ID monster
     private String name;
-    private int value; // Coins yang dibutuhkan untuk mengalahkan monster
+    private int requiredCoins; // Coins yang dibutuhkan untuk mengalahkan monster
     private BinarySearchTree bst;
+    private int value; // Nilai monster
 
-    public Monster(int id, String name, int value) {
+    public Monster(int id, String name, int requiredCoins) {
         this.id = id;
         this.name = name;
-        this.value = value;
-        this.bst = new BinarySearchTree();
+        this.requiredCoins = requiredCoins;
+        this.bst = new BinarySearchTree();  // Inisialisasi Binary Search Tree
     }
 
     // Getter untuk ID monster
@@ -16,31 +17,49 @@ public class Monster {
         return id;
     }
 
+    // Getter untuk nama monster
     public String getName() {
         return name;
     }
 
+    // Getter untuk nilai monster
     public int getValue() {
         return value;
     }
 
+    // Getter untuk coins yang dibutuhkan
+    public int getRequiredCoins() {
+        return requiredCoins;
+    }
+
     // Menambahkan nilai ke dalam BST
-    public void addValueToBST(int value) {
+    public void addVitalObject(int value) {
         bst.insert(value);
     }
 
-    // Metode untuk menyerang monster
+    // Menyerang monster
     public void attack() {
         System.out.println("You are attacking " + name + "!");
     }
 
-    // Metode untuk menemukan objek vital berdasarkan nilai tertentu
+    // Menemukan objek vital berdasarkan nilai tertentu
     public boolean findVitalObject(int attackValue) {
         return bst.search(attackValue); // Mencari apakah attackValue ada dalam BST
     }
 
-    // Metode untuk mengalahkan monster
+    // Mendapatkan depth dari objek vital di BST
+    public int getDepth(int value) {
+        return bst.findDepth(value);
+    }
+
+    // Mengalahkan monster
     public void defeatMonster() {
         System.out.println(name + " has been defeated!");
     }
+
+    // Setter untuk menetapkan nilai monster
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
+
